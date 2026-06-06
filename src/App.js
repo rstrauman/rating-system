630f91d9bb8f60d5a8702f213b22abf58b5c8c5b
Riley <riley.s2003@hotmail.com>
2026-06-06 17:29:15 -0500

    most of modal functionailty complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import StarRating from './components/StarRating';
+import Dialog from './components/Dialog';
+import { useState } from 'react'; 
 
 function App() {
+	const [isOpen, setOpen] = useState(false);
 
 	const stars = [1, 2, 3, 4, 5]
+
+	const openDialog = () => {
+		setOpen(true);
+	}
+
+	const closeDialog = () => {
+		setOpen(false);
+	}
 
 	return (
 		<div className="container stars-container">
 			<StarRating stars={stars}/>
-			<button>Open Dialog</button>
+			<button className="open-btn" onClick={openDialog}>Open Dialog</button>
+			<Dialog closeDialog={closeDialog} isOpen={isOpen}/>
 		</div>
 	);
 }
--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,6 +1,9 @@
-function Dialog() {
+function Dialog(props) {
     return (
-        <></>
+        <div className={props.isOpen ? "open" : "close"}>
+            <h2>Dialog Box</h2>
+            <button className="close" onClick={props.closeDialog}>Close Dialog</button>
+        </div>
     );
 }
 
