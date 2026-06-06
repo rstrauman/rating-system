424d60c03ba18e68b4fdad55e463afbcdbf74ca2
Riley <riley.s2003@hotmail.com>
2026-06-06 16:51:22 -0500

    added h2 and connected functionality to rating

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -4,11 +4,32 @@ import { useState } from 'react';
 function StarRating(props) {
     const [rating, setRating] = useState(0);
 
+    const getText = ()  => {
+        switch (rating) {
+            case  1:
+                return "Boo";
+            case  2:
+                return "Okay";
+            case  3:
+                return "Good";
+            case  4:
+                return "Great";
+            case  5:
+                return "Perfect";
+            default:
+                return "How would you rate this project?";
+        }
+    }
+
     return (
         <section>
-        {props.stars.map(star => (
-            <Star key={star} isClicked={star <= rating} onClick={() => setRating(star)}/>
-        ))}
+            <div className='star-container'>
+            {props.stars.map(star => (
+                <Star key={star} isClicked={star <= rating} onClick={() => setRating(star)}/>
+            ))}
+            </div>
+            <h2>{getText()}</h2>
+            <button>Open Dialog</button>
         </section>
     );
 }
