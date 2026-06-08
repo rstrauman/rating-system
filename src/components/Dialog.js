5f446e35dbfa15b8d75c5e13a6d6b2e878466656
Riley <riley.s2003@hotmail.com>
2026-06-07 21:05:49 -0500

    finished modal styling, app is functinoal

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,9 +1,12 @@
 function Dialog(props) {
     return (
-        <div className={props.isOpen ? "open" : "close"}>
-            <h2>Dialog Box</h2>
-            <button className="close" onClick={props.closeDialog}>Close Dialog</button>
-        </div>
+        <>
+            <div className={props.isOpen ? "overlay" : "close-overlay"}></div>
+            <div className={props.isOpen ? "open" : "close"}>
+                <h2>Dialog Box</h2>
+                <button className="close-btn" onClick={props.closeDialog}>X</button>
+            </div>
+        </>
     );
 }
 
